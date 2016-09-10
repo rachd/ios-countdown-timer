ab00dcaa9fc2a4bdd878eb25d65bde9dd5e97ce6
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-09-10 18:38:18 -0400

    add picker and button

--- CountdownTimer/CountdownTimer/ViewController.m
+++ CountdownTimer/CountdownTimer/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIDatePicker *timer;
+@property (nonatomic, strong) UIButton *startButton;
+
 @end
 
 @implementation ViewController
@@ -17,6 +20,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.timer = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, 40, self.view.frame.size.width, self.view.frame.size.height - 260)];
+    self.timer.datePickerMode = UIDatePickerModeCountDownTimer;
+    [self.view addSubview:self.timer];
+    
+    [self setUpStartButton];
+    
+}
+
+- (void)setUpStartButton {
+    self.startButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.startButton.frame = CGRectMake(20, self.view.frame.size.height - 120, self.view.frame.size.width - 40, 40);
+    [self.startButton setTitle:@"Start Timer" forState:UIControlStateNormal];
+    [self.view addSubview:self.startButton];
 }
 
 - (void)didReceiveMemoryWarning {
