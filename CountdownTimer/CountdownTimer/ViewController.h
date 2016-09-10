dd39b1a1d4ef3ab5d7b475a74559fc76dde33a44
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-09-10 18:13:58 -0400

    initial commit

---
+++ CountdownTimer/CountdownTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CountdownTimer
+//
+//  Created by Rachel Dorn on 9/10/16.
+//  Copyright © 2016 RachelDorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CountdownTimer/CountdownTimer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CountdownTimer
+//
+//  Created by Rachel Dorn on 9/10/16.
+//  Copyright © 2016 RachelDorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
